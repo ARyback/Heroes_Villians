7665caf97e4c348852efeec030f4469c656fba00
ARyback <adam.ryback@gmail.com>
2022-03-27 17:01:26 -0500

    Postman can generate separate lists of heroes and villains

--- supers/views.py
+++ supers/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
+from super_types.models import SuperType
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
@@ -11,15 +12,31 @@ def supers_list(request):
         super_type_name = request.query_params.get('super_type')
         print(super_type_name)
         queryset = Super.objects.all()
-        serializer = SuperSerializer(queryset, many=True)
         if super_type_name:
-            queryset = queryset.filter(super_types__type=super_type_name)
+            queryset = queryset.filter(super_type__type=super_type_name)
+        serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def super_type_list(request):
+    appending_dict_super_type = {}
+    appending_dict_super_type['name'] = 'David'
+    print(appending_dict_super_type)
+    super_types = SuperType.object.all()
+    custom_response_dictionary = {}
+    for super_type in super_types:
+        supers = Super.objects.filter(super_type_id=super_type.id)
+        supers_serializer = SuperSerializer(super, many=True)
+        custom_response_dictionary[super_type.name] = {
+            "type": super_type.type,
+            "supers": supers_serializer.data
+        }
+    return Response(custom_response_dictionary)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
