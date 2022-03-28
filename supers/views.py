c8f53c8c75b5aca2a57d87b89eb7b1e15eea6832
ARyback <adam.ryback@gmail.com>
2022-03-27 21:30:46 -0500

    Final upload of non bonus part of the project

--- supers/views.py
+++ supers/views.py
@@ -34,38 +34,6 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# @api_view(['GET', 'POST'])
-# def supers_list(request):
-#     if request.method == 'GET':
-#         super_type_name = request.query_params.get('super_type')
-#         print(super_type_name)
-#         queryset = Super.objects.all()
-#         if super_type_name:
-#             queryset = queryset.filter(super_type__type=super_type_name)
-#         serializer = SuperSerializer(queryset, many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = SuperSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-# @api_view(['GET'])
-# def super_type_list(request):
-#     appending_dict_super_type = {}
-#     appending_dict_super_type['name'] = 'David'
-#     print(appending_dict_super_type)
-#     super_types = SuperType.object.all()
-#     custom_response_dictionary = {}
-#     for super_type in super_types:
-#         supers = Super.objects.filter(super_type_id=super_type.id)
-#         supers_serializer = SuperSerializer(supers, many=True)
-#         custom_response_dictionary[super_type.name] = {
-#             "type": super_type.type,
-#             "supers": supers_serializer.data
-#         }
-#     return Response(custom_response_dictionary)
-
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
     super = get_object_or_404(Super, pk=pk)
